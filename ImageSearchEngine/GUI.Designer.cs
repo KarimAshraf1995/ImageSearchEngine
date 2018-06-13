5736a12fae95143c094e238b5c4a23a4e4d6ec2f
Karim Ashraf Mohamed Hashish <karimashraf1995@gmail.com>
2018-06-13 18:06:53 +0200

    GUI Logic

--- ImageSearchEngine/GUI.Designer.cs
+++ ImageSearchEngine/GUI.Designer.cs
@@ -30,23 +30,27 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.ImageLocation = new System.Windows.Forms.TextBox();
+            this.ImageSetLocation = new System.Windows.Forms.TextBox();
             this.ImageBrowse_button = new System.Windows.Forms.Button();
             this.ImageLoad_button = new System.Windows.Forms.Button();
             this.FolderLoad_button = new System.Windows.Forms.Button();
             this.FolderBrowse_button = new System.Windows.Forms.Button();
-            this.OriginalpictureBox = new System.Windows.Forms.PictureBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.MatchAcc = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.guess = new System.Windows.Forms.Label();
+            this.guessLabel = new System.Windows.Forms.Label();
             this.StartButton = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.OriginalpictureBox)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.OriginalpictureBox = new System.Windows.Forms.PictureBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.MatchAcc = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.MatchedBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.OriginalpictureBox)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MatchedBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -69,19 +73,19 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "ImageSet";
             // 
-            // textBox1
+            // ImageLocation
             // 
-            this.textBox1.Location = new System.Drawing.Point(80, 13);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(656, 20);
-            this.textBox1.TabIndex = 2;
+            this.ImageLocation.Location = new System.Drawing.Point(80, 13);
+            this.ImageLocation.Name = "ImageLocation";
+            this.ImageLocation.Size = new System.Drawing.Size(656, 20);
+            this.ImageLocation.TabIndex = 2;
             // 
-            // textBox2
+            // ImageSetLocation
             // 
-            this.textBox2.Location = new System.Drawing.Point(80, 48);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(656, 20);
-            this.textBox2.TabIndex = 3;
+            this.ImageSetLocation.Location = new System.Drawing.Point(80, 48);
+            this.ImageSetLocation.Name = "ImageSetLocation";
+            this.ImageSetLocation.Size = new System.Drawing.Size(656, 20);
+            this.ImageSetLocation.TabIndex = 3;
             // 
             // ImageBrowse_button
             // 
@@ -91,6 +95,7 @@
             this.ImageBrowse_button.TabIndex = 4;
             this.ImageBrowse_button.Text = "Browse";
             this.ImageBrowse_button.UseVisualStyleBackColor = true;
+            this.ImageBrowse_button.Click += new System.EventHandler(this.ImageBrowse_button_Click);
             // 
             // ImageLoad_button
             // 
@@ -100,6 +105,7 @@
             this.ImageLoad_button.TabIndex = 5;
             this.ImageLoad_button.Text = "Load";
             this.ImageLoad_button.UseVisualStyleBackColor = true;
+            this.ImageLoad_button.Click += new System.EventHandler(this.ImageLoad_button_Click);
             // 
             // FolderLoad_button
             // 
@@ -118,55 +124,11 @@
             this.FolderBrowse_button.TabIndex = 6;
             this.FolderBrowse_button.Text = "Browse";
             this.FolderBrowse_button.UseVisualStyleBackColor = true;
-            // 
-            // OriginalpictureBox
-            // 
-            this.OriginalpictureBox.Location = new System.Drawing.Point(15, 114);
-            this.OriginalpictureBox.Name = "OriginalpictureBox";
-            this.OriginalpictureBox.Size = new System.Drawing.Size(473, 380);
-            this.OriginalpictureBox.TabIndex = 8;
-            this.OriginalpictureBox.TabStop = false;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(202, 86);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(66, 25);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "Image";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(701, 86);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(147, 25);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "Matched Image";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(538, 114);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(464, 380);
-            this.pictureBox1.TabIndex = 10;
-            this.pictureBox1.TabStop = false;
-            // 
-            // MatchAcc
-            // 
-            this.MatchAcc.AutoSize = true;
-            this.MatchAcc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.MatchAcc.Location = new System.Drawing.Point(977, 86);
-            this.MatchAcc.Name = "MatchAcc";
-            this.MatchAcc.Size = new System.Drawing.Size(0, 25);
-            this.MatchAcc.TabIndex = 12;
+            this.FolderBrowse_button.Click += new System.EventHandler(this.FolderBrowse_button_Click);
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.guess);
+            this.groupBox1.Controls.Add(this.guessLabel);
             this.groupBox1.Location = new System.Drawing.Point(15, 500);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(987, 73);
@@ -174,14 +136,14 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Best Guess";
             // 
-            // guess
+            // guessLabel
             // 
-            this.guess.AutoSize = true;
-            this.guess.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guess.Location = new System.Drawing.Point(473, 26);
-            this.guess.Name = "guess";
-            this.guess.Size = new System.Drawing.Size(0, 31);
-            this.guess.TabIndex = 0;
+            this.guessLabel.AutoSize = true;
+            this.guessLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guessLabel.Location = new System.Drawing.Point(473, 26);
+            this.guessLabel.Name = "guessLabel";
+            this.guessLabel.Size = new System.Drawing.Size(0, 31);
+            this.guessLabel.TabIndex = 0;
             // 
             // StartButton
             // 
@@ -191,33 +153,103 @@
             this.StartButton.TabIndex = 14;
             this.StartButton.Text = "Start";
             this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.OriginalpictureBox);
+            this.groupBox2.Location = new System.Drawing.Point(12, 86);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(510, 408);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(208, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Image";
+            // 
+            // OriginalpictureBox
+            // 
+            this.OriginalpictureBox.Location = new System.Drawing.Point(6, 48);
+            this.OriginalpictureBox.Name = "OriginalpictureBox";
+            this.OriginalpictureBox.Size = new System.Drawing.Size(498, 351);
+            this.OriginalpictureBox.TabIndex = 10;
+            this.OriginalpictureBox.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.MatchAcc);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.MatchedBox1);
+            this.groupBox3.Location = new System.Drawing.Point(543, 86);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(474, 407);
+            this.groupBox3.TabIndex = 16;
+            this.groupBox3.TabStop = false;
+            // 
+            // MatchAcc
+            // 
+            this.MatchAcc.AutoSize = true;
+            this.MatchAcc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MatchAcc.Location = new System.Drawing.Point(442, 16);
+            this.MatchAcc.Name = "MatchAcc";
+            this.MatchAcc.Size = new System.Drawing.Size(0, 25);
+            this.MatchAcc.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(180, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 25);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Matched Image";
+            // 
+            // MatchedBox1
+            // 
+            this.MatchedBox1.Location = new System.Drawing.Point(5, 48);
+            this.MatchedBox1.Name = "MatchedBox1";
+            this.MatchedBox1.Size = new System.Drawing.Size(464, 351);
+            this.MatchedBox1.TabIndex = 13;
+            this.MatchedBox1.TabStop = false;
             // 
             // GUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1025, 585);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.StartButton);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.MatchAcc);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.OriginalpictureBox);
             this.Controls.Add(this.FolderLoad_button);
             this.Controls.Add(this.FolderBrowse_button);
             this.Controls.Add(this.ImageLoad_button);
             this.Controls.Add(this.ImageBrowse_button);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ImageSetLocation);
+            this.Controls.Add(this.ImageLocation);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(1041, 624);
+            this.MinimumSize = new System.Drawing.Size(1041, 624);
             this.Name = "GUI";
             this.Text = "LBP Image Search";
-            ((System.ComponentModel.ISupportInitialize)(this.OriginalpictureBox)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.OriginalpictureBox)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MatchedBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -227,20 +259,22 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox ImageLocation;
+        private System.Windows.Forms.TextBox ImageSetLocation;
         private System.Windows.Forms.Button ImageBrowse_button;
         private System.Windows.Forms.Button ImageLoad_button;
         private System.Windows.Forms.Button FolderLoad_button;
         private System.Windows.Forms.Button FolderBrowse_button;
-        private System.Windows.Forms.PictureBox OriginalpictureBox;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label MatchAcc;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label guess;
+        private System.Windows.Forms.Label guessLabel;
         private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox OriginalpictureBox;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label MatchAcc;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox MatchedBox1;
     }
 }
 
--- ImageSearchEngine/GUI.cs
+++ ImageSearchEngine/GUI.cs
@@ -12,9 +12,84 @@ namespace ImageSearchEngine
 {
     public partial class GUI : Form
     {
+        private bool started = false;
         public GUI()
         {
             InitializeComponent();
+        }
+
+        private void ImageBrowse_button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.CheckFileExists = true;
+            dialog.Filter = "JPEG files|*.jpg|PNG files|*.png";
+            dialog.Title = "Select Image";
+            if ((dialog.ShowDialog()) == DialogResult.OK)
+            {
+                ImageLocation.Text = dialog.FileName;
+            }
+        }
+
+        private void FolderBrowse_button_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "Select Folder";
+
+
+            if ((dialog.ShowDialog()) == DialogResult.OK)
+            {
+                ImageSetLocation.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void ImageLoad_button_Click(object sender, EventArgs e)
+        {
+            if (System.IO.File.Exists(ImageLocation.Text))
+            {
+                OriginalpictureBox.ImageLocation = ImageLocation.Text;
+            }
+            else
+            {
+                MessageBox.Show("Invalid file location");
+            }
+        }
+        private void done()
+        {
+            StartButton.Text = "Start";
+            started = false;
+            ImageLocation.Enabled = true;
+            ImageSetLocation.Enabled = true;
+            ImageLoad_button.Enabled = true;
+            FolderLoad_button.Enabled = true;
+        }
+        public void ImageCompared(string image, double score)
+        {
+            MatchedBox1.ImageLocation = ImageSetLocation + "/" + image;
+            MatchAcc.Text = score.ToString("P2");
+        }
+
+        public void FinalMatch(string image,string guess,double score)
+        {
+            MatchedBox1.ImageLocation = ImageSetLocation + "/" + image;
+            MatchAcc.Text = score.ToString("P2");
+            guessLabel.Text = guess;
+        }
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if (!started)
+            {
+                ImageLocation.Enabled = false;
+                ImageSetLocation.Enabled = false;
+                ImageLoad_button.Enabled = false;
+                FolderLoad_button.Enabled = false;
+                StartButton.Text = "Stop";
+                started = true;
+            }
+            else
+            {
+
+                done();
+            }
         }
     }
 }
